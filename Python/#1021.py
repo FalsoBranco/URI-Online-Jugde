2a123aba1fe7d6c849291c2f4752389d43e6240c
Helter Daniel <helterdaniel@hotmail.com>
2020-10-17 16:25:56 -0300

    fix #1021.py

--- Python/#1021.py
+++ Python/#1021.py
@@ -1,4 +1,4 @@
-"""entrada = float(input(""))
+entrada = float(input(""))
 
 
 notas = [100, 50, 20, 10, 5, 2]
@@ -16,24 +16,4 @@ print("MOEDAS:")
 for moeda in moedas:
     res = int(round(entrada, 2) / moeda)
     print("{:.0f} moeda(s) de R$ {:.2f}".format(res, moeda))
-    entrada = entrada - res * moeda"""
-
-entrada = float(input(""))
-
-
-notas = [100, 50, 20, 10, 5, 2]
-moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
-
-print("NOTAS:")
-for nota in notas:
-    res = entrada // nota
-    print("{:.0f} nota(s) de R$ {}.00".format(res, nota))
-
-    entrada -= round(res * nota, 2)
-
-
-print("MOEDAS:")
-for moeda in moedas:
-    res = round(entrada, 2) // moeda
-    print("{:.0f} moeda(s) de R$ {:.2f}".format(res, moeda))
-    entrada = entrada - res * moeda
+    entrada -= res * moeda
