b5378c4a03308294c8ec1ee1620c3d2bdab76c18
Helter Daniel <helterdaniel@hotmail.com>
2020-10-17 16:26:24 -0300

    fix #1036.py

--- Python/#1036.py
+++ Python/#1036.py
@@ -9,7 +9,7 @@ if delta > 0 and a != 0:
     R1 = (-b + delta ** (1.0 / 2)) / (2 * a)
     R2 = (-b - delta ** (1.0 / 2)) / (2 * a)
 
-    print("R1 ={:.5f}".format(R1))
-    print("R2 ={:.5f}".format(R2))
+    print("R1 = {:.5f}".format(R1))
+    print("R2 = {:.5f}".format(R2))
 else:
     print("Impossivel calcular")
