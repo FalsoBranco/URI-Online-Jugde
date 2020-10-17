18e45d09f51bc9ca579fc11322cecabb1488d5e2
Helter Daniel <helterdaniel@hotmail.com>
2020-10-17 17:10:38 -0300

    ahhhhhh

--- Python/Iniciante/#1037.py
+++ Python/Iniciante/#1037.py
@@ -1,8 +1,12 @@
-valor = float(input(""))
+valor = float(input())
 
-intervalos = ("[0, 25]", "(25, 50]", "(50, 75]", "(75, 100]")
-intervalox = ([0, 25], [25.1, 50.0], [50, 75], [75, 100])
-
-for i, x in enumerate(intervalox):
-    if valor in range(x[0], x[1]):
-        print(intervalos[i])
+if 0 <= valor <= 25:
+    print("Intervalo [0,25]")
+elif 25 <= valor <= 50:
+    print("Intervalo (25,50]")
+elif 50 <= valor <= 75:
+    print("Intervalo (50,75]")
+elif 75 <= valor <= 100:
+    print("Intervalo (75,100]")
+else:
+    print("Fora de intervalo")
